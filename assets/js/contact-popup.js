88788f6bcca1852f75f1b642d3c73ddb2354c2ed
RasmusLeddyr <scud-131@hotmail.com>
2025-05-06 09:45:22 +0200

    JS update + CSS formula

--- assets/js/contact-popup.js
+++ assets/js/contact-popup.js
@@ -3,9 +3,9 @@ const popupElement = document.querySelector("#contact-popup");
 
 // FETCH ALL POPUP BUTTONS
 const popupButtons = document.querySelectorAll(".btn_contact");
-for (let i = 0; i < popupButtons.length; i++) {
-  popupButtons[i].addEventListener("click", clickEvent);
-}
+popupButtons.forEach((item) => {
+  item.addEventListener("click", clickEvent);
+});
 // CLICK FUNCTION
 function clickEvent(clickTarget) {
   popupElement.classList.toggle("active");
